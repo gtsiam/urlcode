7a4d49e563de2977b8342fd2876362b8508b51a2
George Tsiamasiotis <gtsiam@windowslive.com>
2021-06-06 19:30:26 +0300

    Add charset presets

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
 # Url coding utility
 
-This is a convinience tool, for managing urls from the command line.
+This is a convinience tool for managing urls from the command line, inspired by the base\* family of
+tools from `coreutils`.
+
+Examples:
+
+```sh
+$ echo -ne "hello there/ asd23@#%23" | urlcode
+hello%20there%2F%20asd23%40%23%2523
+$ echo -ne "hello%20there%2F%20asd23%40%23%2523" | urlcode -d
+hello there/ asd23@#%23
+```
--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,26 @@ use std::{
     borrow::Cow,
     io::{self, Read, Write},
 };
-use structopt::StructOpt;
+use structopt::{clap::arg_enum, StructOpt};
+
+arg_enum! {
+    #[derive(Debug)]
+    #[allow(non_camel_case_types)]
+    /// Character sets as per whatwg.
+    /// For control: https://url.spec.whatwg.org/#c0-control-percent-encode-set
+    enum CharPreset {
+        control,
+        non_alphanumeric,
+    }
+}
 
 #[derive(Debug, StructOpt)]
 struct Opt {
-    #[structopt(long, short)]
+    #[structopt(long, short, help = "Enable decoding mode")]
     decode: bool,
+
+    #[structopt(long, short, help = "Define charset based on percent-encode set")]
+    preset: Option<CharPreset>,
 }
 
 fn write_bytes(bytes: &[u8]) {
@@ -30,8 +44,12 @@ fn main() {
     if opt.decode {
         write_bytes(&percent_encoding::percent_decode(&input).collect::<Vec<u8>>());
     } else {
+        let charset = match opt.preset.unwrap_or(CharPreset::control) {
+            CharPreset::control => percent_encoding::NON_ALPHANUMERIC,
+            CharPreset::non_alphanumeric => percent_encoding::CONTROLS,
+        };
         write_bytes(
-            percent_encoding::percent_encode(&input, percent_encoding::NON_ALPHANUMERIC)
+            percent_encoding::percent_encode(&input, charset)
                 .collect::<Cow<str>>()
                 .as_bytes(),
         );
