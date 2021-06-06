4778fad44fa4d6086876a0a3dc7db4dff881845a
George Tsiamasiotis <gtsiam@windowslive.com>
2021-06-06 16:00:48 +0300

    Basic implementation

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,39 @@
+use std::{
+    borrow::Cow,
+    io::{self, Read, Write},
+};
+use structopt::StructOpt;
+
+#[derive(Debug, StructOpt)]
+struct Opt {
+    #[structopt(long, short)]
+    decode: bool,
+}
+
+fn write_bytes(bytes: &[u8]) {
+    io::stdout()
+        .write_all(&bytes)
+        .expect("Failed to write to stdout");
+}
+
 fn main() {
-    println!("Hello, world!");
+    let opt = Opt::from_args();
+
+    let input = {
+        let mut vec = Vec::new();
+        io::stdin()
+            .read_to_end(&mut vec)
+            .expect("Failed to read from stdin");
+        vec
+    };
+
+    if opt.decode {
+        write_bytes(&percent_encoding::percent_decode(&input).collect::<Vec<u8>>());
+    } else {
+        write_bytes(
+            percent_encoding::percent_encode(&input, percent_encoding::NON_ALPHANUMERIC)
+                .collect::<Cow<str>>()
+                .as_bytes(),
+        );
+    };
 }
